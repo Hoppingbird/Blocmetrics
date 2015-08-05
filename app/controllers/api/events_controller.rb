88320f766a37f4528c5bc2f6bd5e05fb11f4412c
Hoppingbird <hoppingbird@gmail.com>
2015-08-05 14:34:48 -0400

    client-side js

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,20 +1,4 @@
 class API::EventsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def create
-    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
-    if registered_application.nil?
-      render json: "Unregistered application", status: :unprocessable_entity
-    else
-      # what if name is nil?
-      event = registered_application.events.build(event_params)
-      if event.save
-        render json: @event, status: :created
-      else
-        render @event.errors, status: :unprocessable_entity
-      end
-    end
-  end
 
   before_filter :set_access_control_headers
 
@@ -22,6 +6,27 @@ class API::EventsController < ApplicationController
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+
+  skip_before_action :verify_authenticity_token
+
+  def create
+    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    if registered_application.nil?
+      render json: "Unregistered application", status: :unprocessable_entity
+      # for debugging to see if I hit here or below
+      puts "???????????????????????xxx"
+    else
+      event = registered_application.events.create(event_params)
+      puts "???????????????????????yyy"
+      puts event.to_yaml
+      puts "???????????????????????yyy"
+      if event.save
+        render json: @event, status: :created
+      else
+        render @event.errors, status: :unprocessable_entity
+      end
+    end
   end
 
   private
